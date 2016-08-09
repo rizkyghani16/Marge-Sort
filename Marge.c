b3d0e3d25804b7764779cca2cb97977fe24b45de
Rizky Abdul Ghani S <rizkyghani16@gmail.com>
2016-08-09 20:43:04 +0700

    no message

--- Marge.c
+++ Marge.c
@@ -38,5 +38,9 @@ void merge(int angka[], int memori[], int kiri, int tengah, int kanan)
             	tengah = tengah + 1;
             	simpan = simpan + 1;
         	}
+        	for (i=0; i <= elemen; i++)
+            {
+            	
+        	}
 }
 
